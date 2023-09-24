843235c22e6cec1f5bb85a791a2f3dc8d514e38b
emad22 <emadalhamwi34@gmail.com>
2023-09-24 08:24:06 -0700

    iohiohoi

--- src/App.jsx
+++ src/App.jsx
@@ -22,25 +22,30 @@ function App() {
 
   return (
     <>
-      <BrowserRouter basename="/HalaStore">
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            {/* Replace the existing route with Gametable */}
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/bookstore" element={<BookStore />} />
-            <Route exact path="/about-us" element={<About />} />
-            <Route exact path="/contact-us" element={<Contact />} />
-            <Route exact path="/login" element={<Login />} />
-            <Route exact path="/register" element={<Register />} />
-            <Route exact path="/frequently-asked-questions" element={<FAQ />} />
-            <Route exact path="/shippingpolicy" element={<Return />} />
-            <Route exact path="/storepolicy" element={<StorePlicy />} />
-            <Route exact path="/my-cart" element={<CartPage />} />
-          </Routes>
-        </Suspense>
-      </BrowserRouter>
+    
+    <HashRouter base="/">
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {/* Replace the existing route with Gametable */}
+          <Route exact path="/" element={<Home  />}/>
+          <Route exact path="/bookstore" element={<BookStore />}  />
+          <Route exact path="/about-us" element={<About  />}  />
+          <Route exact path="/contact-us" element={<Contact />}  />
+          <Route exact path="/login" element={<Login />}  />
+          <Route exact path="/register" element={<Register />}  />
+          <Route exact path="/frequently-asked-questions" element={<FAQ />}  />
+          <Route exact path="/shippingpolicy" element={<Return />}  />
+          <Route exact path="/storepolicy" element={<StorePlicy />}  />
+          <Route exact path="/my-cart" element={<CartPage   />}/>
+
+
+
+        </Routes>
+      </Suspense>
+    </HashRouter>
+    
     </>
-  );
+  )
 }
 
 export default App
