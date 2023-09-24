6945af87069c2d2df031c4f1b6038279ca52ade8
emad22 <emadalhamwi34@gmail.com>
2023-09-24 08:19:36 -0700

    dsf

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,13 @@ import { CartProvider } from './CartContext'; // Import your CartProvider
 
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <CartProvider>
+     <CartProvider>
     <App />
     </CartProvider>
   </React.StrictMode>
-)
+);
+
+
