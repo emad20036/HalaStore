77730739de79494c5d8a0d059a29ad82ea030f56
emad22 <emadalhamwi34@gmail.com>
2023-09-25 05:18:56 -0700

    Final Update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/HalaStore",
-  plugins: [react(), chunkSplitPlugin()],
+  plugins: [react()],
   
 })
